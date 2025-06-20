ccc6da0bb427aa39a4953ad2bbead55c209cfb32
brenda <dollanganger.8@gmail.com>
2025-06-20 15:55:19 -0300

    Initial commit

---
+++ src/main/java/com/example/quickvote/QuickvoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quickvote/QuickvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
